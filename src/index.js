c7f0d01c34f1aaeed701a74c4081a0cb0f115593
Jorge-William <jorge_william_cardoso@yahoo.com.br>
2022-01-06 11:29:20 -0300

    Setting the server

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
 const express = require('express')
 const app = express()
+require('./db/mongoose')
+const User = require('./model/user')
+const Task = require('./model/task')
 
-const port = process.env.PORT || 3000
+app.use(express.json())
+
+const port = process.env.PORT || 3001
 
 app.post('/users', (req, res) => {
-  res.send('Testing!')
+	const user = new User(req.body)
+
+	user.save()
+		.then(
+			(user) => {
+				res.status(201).send(user)
+				console.log('User saved!!')
+			}
+		)
+		.catch(
+			(error) => {
+				res.status(400).send(error)
+			}
+		)
+
 })
 
+app.post('/tasks', (req, res) => {
+	const task = new Task(req.body)
+
+	task.save().then((task) => {
+		res.status(201).send(task)
+	}).catch((error) => {
+		res.status(400).send(error)
+	})
+
+})
+
+
 app.listen(port, () => {
-  console.log('Server running on port', 3000)
+	console.log('Server running on port', port)
 })
