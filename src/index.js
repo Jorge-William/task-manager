1142b379fd922419db4c1bac8c53093146b46413
Jorge-William <jorge_william_cardoso@yahoo.com.br>
2022-01-10 15:49:55 -0300

    Feat: Changing - promises to async await

--- src/index.js
+++ src/index.js
@@ -1,78 +1,84 @@
-const express = require('express')
-const app = express()
-require('./db/mongoose')
-const User = require('./model/user')
-const Task = require('./model/task')
+const express = require('express');
+const app = express();
+require('./db/mongoose');
+const User = require('./model/user');
+const Task = require('./model/task');
 
-app.use(express.json())
+app.use(express.json());
 
-const port = process.env.PORT || 3001
+const port = process.env.PORT || 3001;
 
 // ------------------ Cria um novo usuário
-app.post('/users', (req, res) => {
-	const user = new User(req.body)
-
-	user.save()
-		.then(
-			(user) => {
-				res.status(201).send(user)
-				console.log('User saved!!')
-			}
-		)
-		.catch(
-			(error) => {
-				res.status(400).send(error)
-			}
-		)
-})
+app.post('/users', async (req, res) => {
+  const user = new User(req.body);
+  try {
+    await user.save();
+    res.status(201).send(user);
+    console.log('User saved!!');
+  } catch (error) {
+    await res.status(400).send(error);
+  }
+});
 
 // --------------------- Cria uma nova tarefa
-app.post('/tasks', (req, res) => {
-	const task = new Task(req.body)
-
-	task.save().then((task) => {
-		res.status(201).send(task)
-	}).catch((error) => {
-		res.status(400).send(error)
-	})
-})
+app.post('/tasks', async (req, res) => {
+  const task = new Task(req.body);
+  try {
+    await task.save();
+    res.status(201).send(task);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
 
 // ------------------ Busca todos os usuários do banco de dados (tipo select * no sql)
-app.get('/users', (req, res) => {
-	User.find({}).then((queryResult) => {
-		res.send(queryResult)
-	}).catch((error) => {
-		res.status(500).send()
-	})
-})
+app.get('/users', async (req, res) => {
+  try {
+    const result = await User.find({});
+    res.send(result);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
 
 // ------------------ Busca um usuário específico no banco de dados
-app.get('/users/:id', (req, res) => {
-	const _id = req.params.id
-
-	User.findById(_id).then((result) => {
-		if (!result) {
-			return res.status(404).send()
-		}
-		res.status(200).send(result)
-	}).catch((error) => {
-		res.status(404).send(error)
-	})
-})
+app.get('/users/:id', async (req, res) => {
+  const _id = req.params.id;
+  try {
+    const user = await User.findById(_id);
+    if (!user) {
+      return res.status(404).send(error);
+    }
+    res.status(200).send(user);
+  } catch (error) {
+    return res.status(404).send(error);
+  }
+});
 
 // ------------------ Busca todas as tarefas
-app.get('/tasks', (req, res) => {
-	Task.find().then((results) => {
-		res.status(200).send(results)
-	}).catch((error) => {
-		res.status(404).send(error)
-	})
-})
+app.get('/tasks', async (req, res) => {
+  try {
+    const tasks = await Task.find();
+    res.status(404).send(tasks);
+  } catch (error) {
+    res.status(404).send(error);
+  }
+});
 
-
-
-
+// ------------------ Busca tarefa por _id
+app.get('/task/:id', async (req, res) => {
+  const _idTask = req.params.id;
+  try {
+    const task = await Task.findById(_idTask);
+    if (!task) {
+      return res.status(404).send();
+    }
+    res.status(200).send(task);
+  } catch (error) {
+    res.status(404).send(error);
+  }
+});
 
 app.listen(port, () => {
-	console.log('Server running on port', port)
-})
+  console.log('Server running on port', port);
+});
