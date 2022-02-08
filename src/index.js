6ed4ff97d343ec35a6a077ed3d37cb4cbd6d8ea2
Jorge William <jorge_william_cardoso@yahoo.com.br>
2022-02-08 11:58:03 -0300

    Feat: Gerando token

--- src/index.js
+++ src/index.js
@@ -14,14 +14,3 @@ const port = process.env.PORT || 3000
 app.listen(port, () => {
 	console.log('Server running on port', port)
 })
-
-// const hashingPassword = async (plainText) => {
-// 	const hashedPassword = await bcrypt.hash(plainText, 8)
-// 	const otherPassword = 'JW2019dan133'
-// 	const hashToPlain = await bcrypt.compare(otherPassword, hashedPassword)
-
-// 	console.log(hashedPassword)
-// 	console.log(hashToPlain)
-// }
-
-// hashingPassword('JW2019dan132')
--- src/routers/user.js
+++ src/routers/user.js
@@ -7,7 +7,9 @@ router.post('/user', async (req, res) => {
 	const user = new User(req.body)
 	try {
 		await user.save()
-		res.status(201).send(user)
+		const token = user.generateAuthToken()
+
+		res.status(201).send({ name, token })
 		console.log('User saved!!')
 	} catch (error) {
 		await res.status(400).send(error)
@@ -21,9 +23,11 @@ router.post('/user/login', async (req, res) => {
 			req.body.email,
 			req.body.password
 		)
-		res.send(user)
+		const token = await user.generateAuthToken()
+
+		res.status(201).send({ user, token })
 	} catch (error) {
-		res.status(400).send(error)
+		res.status(400).send({ error: error })
 	}
 })
 
